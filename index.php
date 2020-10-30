cbf80c531bf5ea8d8da52cfc2f5149685024885a
Ratchanee12 <73681315+Ratchanee12@users.noreply.github.com>
2020-10-30 12:28:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <html>
   <head>
-    <title>รัชนีย์ ขันธิวงค์ ชื่อเล่น น้อยหน่า</title>
+    <meta charset='UTF-8'>
+    <title>Web</title>
   </head>
   <body>
+    <h1><center>รัชนีย์ ขันธิวงค์</center></h1>
+    <h2><center>ชื่อเล่น น้อยหน่า</center></h2>
+    <br />
+    <img src="cat.jpg" />
+        <br />
+    
+    
+    
+    
     
    </body>
   </html>
