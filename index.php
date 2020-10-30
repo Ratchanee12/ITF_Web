8c1d253feddba7a55c3e4da38e76325c84c5d16c
Ratchanee12 <73681315+Ratchanee12@users.noreply.github.com>
2020-10-30 12:43:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,15 @@
   <body>
     <h1><center>รัชนีย์ ขันธิวงค์</center></h1>
     <h2><center>ชื่อเล่น น้อยหน่า</center></h2>
-    <br />
-    <img src="cat.jpg" />
-        <br />
-    
-    
-    
-    
-    
+    <img src="cat1.jpg">
+    <br>
+    <img src="cat2.jpg" />
+    <a href="http://www.it.kmitl.ac.th">This is a Link</a>
+    </ul>
+        <li>วันก่อนเข้าเซเว่นล้มเลย เจอซุปไก่สกัด</li>
+        <li>A : เมื่อเช้าเปิดตู้ยาดู ตกใจสัส! เกือบหลบไม่ทัน!!
+        <li>B : ยาหล่นใส่เท้า?</li>
+        <li>A : ป่าว กูเจอกระต่ายบิน!!</li>
+    </ul>
    </body>
   </html>
